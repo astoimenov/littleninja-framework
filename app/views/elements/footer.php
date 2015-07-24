6dfde152f987140de7786538b85f5fefc11a7d29
Alexander Stoimenov <alexander.stoimenov@outlook.com>
2015-07-24 08:27:48 +0200

    Small changes

--- app/views/elements/footer.php
+++ app/views/elements/footer.php
@@ -1,4 +1,3 @@
-
         </main>
         <footer class="footer navbar-fixed-bottom text-center">
             <div class="container">
@@ -9,8 +8,8 @@
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/select2/4.0.0/js/select2.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="script/jquery.mousewheel.js"></script>
-    <script type="text/javascript" src="script/jquery.jscrollpane.min.js"></script>
+    <script type="text/javascript" src="../../../script/jquery.mousewheel.js"></script>
+    <script type="text/javascript" src="../../../script/jquery.jscrollpane.min.js"></script>
     <script>
         $(function () {
             $('body').jScrollPane();
